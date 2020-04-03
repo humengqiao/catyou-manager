4b0d3a162c4c20115b8c7757f8733d204a0e3df7
humengqiao <573071547@qq.com>
2020-04-03 22:48:27 +0800

    refactor: 2020-04-04默哀逻辑修改

--- src/activitys/silentMourning.js
+++ src/activitys/silentMourning.js
@@ -7,12 +7,12 @@ export default {
 			const month = date.getMonth()
 			const day = date.getDate()
 
-			if(year === 2020 && (month + 1) === 4 && day === 3) {
+			if(year === 2020 && (month + 1) === 4 && day === 4) {
 				if(!document.documentElement.classList.contains('silent-mourning')) {
 					document.documentElement.classList.add('silent-mourning')
 				}
 			}else {
-				if(year > 2020 || (year === 2020 && (month + 1) > 3) || (year === 2020 && (month + 1) > 4 && day > 3)) {
+				if(year > 2020 || (year === 2020 && (month + 1) > 4) || (year === 2020 && (month + 1) > 4 && day > 4)) {
 					return clearInterval(window.mourningTimer)
 				}
 
@@ -20,6 +20,6 @@ export default {
 					document.documentElement.classList.remove('silent-mourning')
 				}
 			}
-		}, 1000 * 10)
+		}, 1000 * 60)
 	}
 }
